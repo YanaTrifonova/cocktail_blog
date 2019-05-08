4a77fbef7fb9d5402614d8c9d8b85ab29b0473e6
Iana <trifonova.yana.prof@gmail.com>
2019-05-08 22:32:57 +0300

    init UI commit

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+
+function css() {
+    return gulp
+        .src('scss/main.scss')
+        .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('static/stylesheet'))
+}
+
+function watchFiles() {
+    gulp.watch('scss/*.scss', css);
+    gulp.watch('templates/index.html')
+}
+
+gulp.task('css', css);
+gulp.task('watch', gulp.parallel(watchFiles));
