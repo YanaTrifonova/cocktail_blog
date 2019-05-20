a661d152ee30a2e8df9e4cbce16fb34a3f3c88a2
Iana <trifonova.yana.prof@gmail.com>
2019-05-20 21:18:35 +0300

    add cocktail name in post html

---
+++ static/javascript/post/checkboxExpansion.js
@@ -0,0 +1,6 @@
+$(document).ready(function () {
+    $('#isCocktail').change(function () {
+        $('#isCocktailName').toggle();
+    });
+})
+
---
+++ static/javascript/post/summernoteEditField.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('#note').summernote();
+});
+var edit = function () {
+    $('.click2edit').summernote({focus: true});
+};
+
+var save = function () {
+    var markup = $('.click2edit').summernote('code');
+    $('.click2edit').summernote('destroy');
+};
