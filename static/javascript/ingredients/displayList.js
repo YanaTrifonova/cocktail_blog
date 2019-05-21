b9878863984c1607fe8b536326c0a1f36b8e612b
Iana <trifonova.yana.prof@gmail.com>
2019-05-21 16:10:50 +0300

    add display ingredients of the cocktails on page

---
+++ static/javascript/ingredients/displayList.js
@@ -0,0 +1,22 @@
+$(document).ready(function () {
+    $.ajax({
+        url: '/cocktails',
+        type: 'GET',
+        dataType: "json",
+        success: function (data) {
+            let cocktails = data['cocktails'];
+            let ingredient = Object.keys(cocktails);
+            for (var x in ingredient) {
+                $("#list").append(`<div id=${ingredient[x]} class='cocktailList__container'></div>`);
+                let containerSelector = $('#' + ingredient[x]);
+                containerSelector.append(`<h2 class='cocktailList__container__header'>${ingredient[x]}</h2>`);
+                $(cocktails[ingredient[x]]).each(function () {
+                    let id = this.id;
+                    let cocktailName = this.name;
+                    containerSelector.append(`<p class='cocktailList__container__name' 
+                                                 onclick="articleDisplay('${id}')">${cocktailName}</p>`);
+                })
+            }
+        },
+    })
+});
