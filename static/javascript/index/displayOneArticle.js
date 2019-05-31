880edb07252e883e3ccf3118722ffee81d58313b
Dmitry Kalashnikov <dmitry@calibermind.com>
2019-05-31 16:39:27 +0300

    correct link to display single  article

--- static/javascript/index/displayOneArticle.js
+++ static/javascript/index/displayOneArticle.js
@@ -1,7 +1,7 @@
 /*Display one particular article that has been chosen by user*/
 
 function articleDisplay(idToDisplay) {
-    window.location.href = '/' + idToDisplay;
+    window.location.href = '/posts/' + idToDisplay;
 
     let hiddenPrev = document.getElementById("prev");
     hiddenPrev.className += " paginations__hidden";
